9e3331d6ddc18caa47796b5ab2e6d61b40b2f158
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-22 18:20:57 -0300

    Estrutura if

---
+++ Au2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    setlocale(LC_ALL, "portuguese");
+    string nome;
+    int idade;
+
+    cout<<"Digite seu nome: ";
+    cin>>nome;
+    cout<<"Digite sua idade: ";
+    cin>>idade;
+
+    if(idade >= 18){
+        cout<<nome<<" pela lei você já responde com um adulto.";
+    }else{
+        cout<<nome<<" pela lei você ainda e menor de idade.";
+    }
+
+    return 0;
+}
