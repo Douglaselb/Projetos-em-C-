f1252911f88c3fd30a67e6cb005089b44b51dbb1
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-22 20:53:22 -0300

    Estrutura while
    Estrutura simples do while com incremento.

---
+++ Au2.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x = 0;
+    while ( x < 5){
+        //x = 0;
+        cout<<x<<endl;
+        x++;
+    }
+    
+    return 0;
+}
