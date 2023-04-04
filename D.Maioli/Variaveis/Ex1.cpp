1e615f8fc1a388995e341859a502a308f92b598d
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-04-03 21:59:25 -0300

    Declarando variável

---
+++ D.Maioli/Variaveis/Ex1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int valor = 5;
+    int var2;
+    cout<<"VAlor da variavel ja determinada: "<<valor<<endl;
+    cout<<"Informe o valor da segunda variavel: ";
+    cin>> var2;
+    cout<<"Valor infomrado: "<<var2;
+
+    return 0;
+}
