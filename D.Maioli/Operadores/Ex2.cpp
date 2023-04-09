a7ae8941cfebbd57a83d3ab375df057d59a05806
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-04-08 22:20:59 -0300

    Par ou impar

--- D.Maioli/Operadores/Ex2.cpp
+++ D.Maioli/Operadores/Ex2.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int main(int argc, char const *argv[]){
     int num;
-    cout<<"\tAnalisador de números"<<endl<<"Informe um valor inteiro: ";
+    cout<<"\tAnalisador de numeros"<<endl<<"Informe um valor inteiro: ";
     cin>>num;
+    (num % 2 == 0) ? cout<<"O numero informado e par" : cout<<"O numero e impar";
+    /*
     if (num % 2 == 0)
         cout<<"O número informado é par";
     else
         cout<<"O número informado é impar";
+    */
     return 0;
 }
