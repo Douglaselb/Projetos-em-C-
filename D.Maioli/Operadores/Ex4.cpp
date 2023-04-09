b1595adfaf99dba2e179c77bd8d10543fb7f0562
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-04-09 11:50:31 -0300

    Hipotenusa

---
+++ D.Maioli/Operadores/Ex4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(int argc, char const *argv[]){
+    float catetoad, catetoop, hipotenusa, ca, co;
+    cout<<"Para calcular a hipotenusa favor informar os valores abaixo";
+    cout<<endl<<"Informe o valor do cateto oposto: ";
+    cin>>catetoop;
+    cout<<"Informe o valor do cateto adjacente: ";
+    cin>>catetoad;
+    cout<<"Segue os valores:\nCateto Oposto: "<<catetoop<<"\nCateto adjacente : "<<catetoad<<"\nHipotenusa: ";
+    cout<<hypot(catetoad, catetoop);
+    return 0;
+}
